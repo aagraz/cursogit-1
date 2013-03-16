012b4ab6c12688bb49a7ee7b98f125cf8ecdd349
Alfonso Alba <aalbagarcia@gmail.com>
2013-03-16 12:21:09 +0100

    Añadimos clase evento (vacía)

---
+++ GitEvento.h
@@ -0,0 +1,13 @@
+//
+//  GitEvento.h
+//  cursogit
+//
+//  Created by Alfonso Alba on 16/03/13.
+//  Copyright (c) 2013 Alfonso Alba. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GitEvento : NSObject
+
+@end
---
+++ GitEvento.m
@@ -0,0 +1,13 @@
+//
+//  GitEvento.m
+//  cursogit
+//
+//  Created by Alfonso Alba on 16/03/13.
+//  Copyright (c) 2013 Alfonso Alba. All rights reserved.
+//
+
+#import "GitEvento.h"
+
+@implementation GitEvento
+
+@end
