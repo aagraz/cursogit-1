6a8d363d3ef9d0a65c5bfeb6d9a3bdbc69f4e15c
Alfonso Alba <aalbagarcia@gmail.com>
2013-03-16 12:05:16 +0100

    Añadiendo la clase alumnos (vacía)

---
+++ GitAlumno.h
@@ -0,0 +1,13 @@
+//
+//  GitAlumno.h
+//  cursogit
+//
+//  Created by Alfonso Alba on 16/03/13.
+//  Copyright (c) 2013 Alfonso Alba. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GitAlumno : NSObject
+
+@end
---
+++ GitAlumno.m
@@ -0,0 +1,13 @@
+//
+//  GitAlumno.m
+//  cursogit
+//
+//  Created by Alfonso Alba on 16/03/13.
+//  Copyright (c) 2013 Alfonso Alba. All rights reserved.
+//
+
+#import "GitAlumno.h"
+
+@implementation GitAlumno
+
+@end
