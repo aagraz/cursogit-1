6c8e72145b60db5b17c46323f9cfb4c5800be80e
Alfonso Alba <aalbagarcia@gmail.com>
2013-03-16 15:34:07 +0100

    Creando EventoViewController

---
+++ cursogit/GitEventoViewController.h
@@ -0,0 +1,16 @@
+//
+//  GitEventoViewController.h
+//  cursogit
+//
+//  Created by Alfonso Alba on 16/03/13.
+//  Copyright (c) 2013 Alfonso Alba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GitEvento.h"
+
+@interface GitEventoViewController : UIViewController
+
+@property (nonatomic, strong) GitEvento *evento;
+
+@end
---
+++ cursogit/GitEventoViewController.m
@@ -0,0 +1,38 @@
+//
+//  GitEventoViewController.m
+//  cursogit
+//
+//  Created by Alfonso Alba on 16/03/13.
+//  Copyright (c) 2013 Alfonso Alba. All rights reserved.
+//
+
+#import "GitEventoViewController.h"
+
+@interface GitEventoViewController ()
+
+@end
+
+@implementation GitEventoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
